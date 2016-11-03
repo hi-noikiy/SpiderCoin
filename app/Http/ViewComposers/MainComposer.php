dcef99f6633c454748aa049eb62fb63ef6b082a1
zihao <yaizhao@yaozihao.cn>
2016-11-03 21:36:38 +0800

    add backend

--- app/Http/ViewComposers/MainComposer.php
+++ app/Http/ViewComposers/MainComposer.php
@@ -18,18 +18,18 @@ class MainComposer
     public function compose(View $view)
     {
         $route = Route::currentRouteName();
-//        $menus = MenuRepository::getAllDisplayMenus();
+        $menus = MenuRepository::getAllDisplayMenus();
 
-        $menus = [
-            // 主管理模块
-            ['id'=>1,'name'=>'dingtou.menu.group','description'=>'操作管理','route'=>'backend.index','parent_id'=>0,'hide'=>0,'icon'=>12,'sort'=>12],
-//            ['id'=>5,'name'=>'dingtou.menu.group','description'=>'操作管理','route'=>'backend.menu','parent_id'=>3,'hide'=>0,'icon'=>12,'sort'=>12],
-            ['id'=>2,'name'=>'dingtou.menu.group','description'=>'操作管理','route'=>'backend.index','parent_id'=>1,'hide'=>0,'icon'=>12,'sort'=>12],
-//            ['id'=>3,'name'=>'dingtou.menu.group','description'=>3,'route'=> 'backend.menu','parent_id'=>1,'hide'=> 0,'icon'=> 12,'sort'=> 12,],
-
-            // 二级模块
-
-        ];
+//        $menus = [
+//            // 主管理模块
+//            ['id'=>1,'name'=>'dingtou.menu.group','description'=>'操作管理','route'=>'backend.index','parent_id'=>0,'hide'=>0,'icon'=>12,'sort'=>12],
+////            ['id'=>5,'name'=>'dingtou.menu.group','description'=>'操作管理','route'=>'backend.menu','parent_id'=>3,'hide'=>0,'icon'=>12,'sort'=>12],
+//            ['id'=>2,'name'=>'dingtou.menu.group','description'=>'操作管理','route'=>'backend.index','parent_id'=>1,'hide'=>0,'icon'=>12,'sort'=>12],
+////            ['id'=>3,'name'=>'dingtou.menu.group','description'=>3,'route'=> 'backend.menu','parent_id'=>1,'hide'=> 0,'icon'=> 12,'sort'=> 12,],
+//
+//            // 二级模块
+//
+//        ];
         $title = $this->getPageDescriptionArrayByMenus($menus);
         $view->with( compact('menus', 'route', 'title') );
     }
